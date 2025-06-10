65e704ea225af05e931bdc2f00250946732c9f80
unknown <pawanbwagh@gmail.com>
2025-06-10 23:01:07 +0530

    pawan

---
+++ Intoduction.js
@@ -0,0 +1,50 @@
+//------Java Script is written in Types--------//
+
+// 1.Inline
+// 2.Internal
+// 3.External
+
+//--- Inline ---
+// Written inside two point bracket same as attributes
+
+//--- Internal --- 
+// Written inside a body tag.
+
+//--- External ---
+//--- We have to create a external folder Eg:-(basic.js) and add the head tag of html file Eg-(<script src="./basic.js"></script>)
+
+//---- Types of Printng Data---
+// 1.alert()
+// 2.document.write()
+// 3.console.log()
+
+//--- 1.alert ----
+// gives on "pop up" message on web page
+
+//--- 2.document.write----
+// It directly write the content on web page
+
+//--- 3.console.log ----
+// It is use to print the message in console of Browser
+
+//---- Types of key-words in JS----
+// 1.var :- use to Declare, Reassign, Redeclare the object
+// 2.let :- use to Declare, Reassign the object
+// 3.const :- use to Declare the object
+
+//---- Types of Data types in JS ----
+// 1.premative
+// 2.non-premative
+
+//--- 1.premative types:-
+//--I, null
+//--II, number
+//--III, boolen
+//--IV, bigint 
+//--V, string
+//--VI, Symbol
+//--VII, undefined
+
+//--- 2.Non Premative in JS :- 
+//--I Array 
+//--II object 
